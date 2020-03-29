228ecfcb9eccf4d80e1b30a765257c69b46ca89a
Adil-io <adilyusuf786@gmail.com>
2020-03-29 10:49:49 +0530

    first commit

---
+++ chatapp-backend/controllers/users.js
@@ -0,0 +1,58 @@
+const HttpStatus = require('http-status-codes');
+
+const User = require('../models/userModels');
+
+module.exports = {
+	async GetAllUsers(req, res) {
+		await User.find({})
+			.populate('posts.postId')
+			.populate('following.userFollowed')
+			.populate('followers.followed')
+			.populate('chatList.receiverId')
+			.populate('chatList.msgId')
+			.then(result => {
+				res
+					.status(HttpStatus.OK)
+					.json({ message: 'Getting All users', result });
+			})
+			.catch(err => {
+				res
+					.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.json({ message: 'Error getting all users', result });
+			});
+	},
+
+	async GetUser(req, res) {
+		await User.findOne({ _id: req.params.id })
+			.populate('posts.postId')
+			.populate('following.userFollowed')
+			.populate('followers.follower')
+			.populate('chatList.receiverId')
+			.populate('chatList.msgId')
+			.then(result => {
+				res.status(HttpStatus.OK).json({ message: 'User by id', result });
+			})
+			.catch(err => {
+				res
+					.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.json({ message: 'Error getting user by id' });
+			});
+	},
+
+	async GetUserByName(req, res) {
+		await User.findOne({ username: req.params.username })
+			.populate('posts.postId')
+			.populate('following.userFollowed')
+			.populate('followers.follower')
+			.populate('chatList.receiverId')
+			.populate('chatList.msgId')
+			.then(result => {
+				res.status(HttpStatus.OK).json({ message: 'User by name', result });
+			})
+			.catch(err => {
+				res
+					.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.json({ message: 'Error getting user by name' });
+			});
+	}
+};
---
+++ chatapp/src/app/services/users.service.ts
@@ -0,0 +1,54 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+const BASEURL = 'http://localhost:3000/api/chatapp';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class UsersService {
+	constructor(private http: HttpClient) {}
+
+	GetAllUsers(): Observable<any> {
+		return this.http.get(`${BASEURL}/users`);
+	}
+
+	// To return a Promise handle it with then() instead of subscribe()
+	// async GetAllUsers(){
+	//   return await this.http.get(`${BASEURL}/users`)
+	// }
+
+	GetUserById(id): Observable<any> {
+		return this.http.get(`${BASEURL}/user/${id}`);
+	}
+
+	GetUserByName(username): Observable<any> {
+		return this.http.get(`${BASEURL}/username/${username}`);
+	}
+
+	FollowUser(userFollowed): Observable<any> {
+		return this.http.post(`${BASEURL}/follow-user`, {
+			userFollowed
+		});
+	}
+
+	UnFollowUser(userFollowed): Observable<any> {
+		return this.http.post(`${BASEURL}/unfollow-user`, {
+			userFollowed
+		});
+	}
+
+	MarkNotification(id, deleteValue?): Observable<any> {
+		return this.http.post(`${BASEURL}/mark/${id}`, {
+			id,
+			deleteValue
+		});
+	}
+
+	MarkAllAsRead(): Observable<any> {
+		return this.http.post(`${BASEURL}/mark-all`, {
+			all: true
+		});
+	}
+}
